35d5fb4a540ae981680fbf25d439ce4c0d1e0e34
BacHaddad <br.yhaddad@gmail.com>
2021-08-05 15:27:13 +0200

    signup form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,9 +1,10 @@
 <template>
-  <form>
+  <form @submit.prevent>
     <label> Email:</label>
-    <input type="email" v-model="email" required />
+    <input type="email" v-model="email"  required />
     <label> Password:</label>
-    <input type="password" v-model="password" required />
+    <p class="error" v-if="passwordError" > {{passwordError}} </p>
+    <input type="password" v-model="password" @keyup="handleSubmitForm" required />
     <label>Role:</label>
     <select v-model="role">
       <option value="developer"> Web Developer </option>
@@ -11,41 +12,42 @@
     </select>
 
     <label> Skills: </label>
-    <input type="text"  v-model="tempSkill" @keyup="addSkill" placeholder="use ',' to separate your skills ">
-    <div v-for=" skill in skills" :key="skill" class="pill" >
-      <span @click="deleteSkill(skill)" > {{skill}}</span>
+    <input
+      type="text"
+      v-model="tempSkill"
+      @keyup="addSkill"
+      placeholder="use ',' to separate your skills "
+    />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)"> {{ skill }}</span>
     </div>
-    
+
     <div class="terms">
       <input type="checkbox" v-model="terms" required />
       <label> Accept terms and conditions </label>
     </div>
 
-
-
     <div>
-     <p> Add interests </p>
-      <input type="checkbox" value="sports" v-model="names" />
+      <p>Add interests</p>
+      <input type="checkbox" value="sports" v-model="interest" />
       <label> Sports </label>
-    </div>
-    <div>
-      <input type="checkbox" value="food" v-model="names" />
+      <input type="checkbox" value="food" v-model="interest" />
       <label> Food </label>
-    </div>
-    <div>
-      <input type="checkbox" value="travel" v-model="names" />
+      <input type="checkbox" value="travel" v-model="interest" />
       <label> Travel </label>
+       <input type="checkbox" value="music" v-model="interest" />
+      <label> Music </label>
     </div>
 
-    <div class="submi">
-      <button> Create an account </button>
+    <div class="submit">
+      <button>Create an account</button>
     </div>
   </form>
   <p>your email: {{ email }}</p>
   <p>your password: {{ password }}</p>
   <p>your role: {{ role }}</p>
   <p>Accepted terms and conditions: {{ terms }}</p>
-  <p>Names: {{ names }}</p>
+  <p>Interests: {{ interest }}</p>
   <p>skills: {{ skills }}</p>
 </template>
 
@@ -58,31 +60,38 @@ export default {
       password: "",
       role: "designer",
       terms: false,
-      names: [],
-      tempSkill: '',
-      skills: []
+      interest: [],
+      tempSkill: "",
+      skills: [],
+      passwordError: "",
     };
   },
 
   methods: {
     addSkill(e) {
-      if(e.key === ","  && this.tempSkill) {
-        const newTempSkill = this.tempSkill.replace(',', '')
-        if(newTempSkill !== '' && newTempSkill !== ','  ) {
-        const skillToUpperCase = newTempSkill.toUpperCase()
-        if(!this.skills.includes(skillToUpperCase)) {
-          this.skills.push(skillToUpperCase)
-        }
-        this.tempSkill = ''
+      if (e.key === "," && this.tempSkill) {
+        const newTempSkill = this.tempSkill.replace(",", "");
+        if (newTempSkill !== "" && newTempSkill !== ",") {
+          const skillToUpperCase = newTempSkill.toUpperCase();
+          if (!this.skills.includes(skillToUpperCase)) {
+            this.skills.push(skillToUpperCase);
+          }
+          this.tempSkill = "";
         }
       }
     },
 
     deleteSkill(skill) {
-     this.skills = this.skills.filter(el=> el !== skill)
+      this.skills = this.skills.filter((el) => el !== skill);
+    },
 
-    }
-  }
+    handleSubmitForm() {
+      this.passwordError =
+        this.password.length > 6
+          ? ""
+          : "the password must be at least 6 characters long";
+    },
+  },
 };
 </script>
 
@@ -98,8 +107,8 @@ form {
 label {
   color: #4b4b4b;
   display: inline-block;
-  margin: 25px 0 15px;
-  font-size: 0.6em;
+  margin: 25px 20px 15px 0;
+  font-size: 0.8em;
   text-transform: uppercase;
   letter-spacing: 1px;
   font-weight: bold;
@@ -117,33 +126,39 @@ select {
 input[type="checkbox"] {
   display: inline-block;
   width: 16px;
-  margin: 0 10px 0 0;
+  margin: 0 7px 0 0;
   position: relative;
   top: 2px;
 }
 .pill {
-display: inline-block;
-margin: 20px 10px 0 0 ;
-padding: 6px 12px ;
-background-color: #eee;
-border-radius: 20px;
-font-size: 12px;
-letter-spacing: 1px;
-font-weight: bold;
-color: #777;
-cursor: pointer;
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background-color: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 
-button{
+button {
   background-color: #0b6dff;
   border: 0;
   padding: 10px 20px;
   margin-top: 20px;
   color: white;
   border-radius: 20px;
-
-  }
-.submit{
+}
+.submit {
   text-align: center;
+}
+.error{
+  color: #c0004a;
+  margin: 10px;
+  font-size: .8em;
+  font-weight: bold;
+
 }
 </style>
